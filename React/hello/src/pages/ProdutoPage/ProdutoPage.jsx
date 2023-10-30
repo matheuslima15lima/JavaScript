c703b07d046f116ecc7da27274ce7e9ef5217217
Matheus <matheus.cavalcanti5@aluno.senai.br>
2023-10-30 17:16:31 -0300

    Feito Paginas home login produto

---
+++ React/hello/src/pages/ProdutoPage/ProdutoPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProdutoPage = () => {
+    return (
+        <div>
+           <h1>Pagina de Produtos</h1> 
+        </div>
+    );
+};
+
+export default ProdutoPage;

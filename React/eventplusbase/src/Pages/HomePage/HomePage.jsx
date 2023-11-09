a1709fb1140c950c88f17f8bfaa1f4b34aaa7c18
Matheus <matheus.cavalcanti5@aluno.senai.br>
2023-11-09 17:24:49 -0300

    useEffect

--- React/eventplusbase/src/Pages/HomePage/HomePage.jsx
+++ React/eventplusbase/src/Pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainContent from '../../components/MainContent/MainContent';
 import Banner from '../../components/Banner/Banner';
 import VisionSection from '../../components/VisionSection/VisionSection';
@@ -7,6 +7,16 @@ import NextEvent from '../../components/NextEvent/NextEvent';
 import Container from '../../components/Container/Container'
 import './HomePage.css'
 const HomePage = () => {
+
+    useEffect(()=> {
+        //continuar aqui
+    } ,[])
+    //fake mock - api mocada
+    const [nextEvents, setNextEvents] = useState([
+      {id: 1, title: "Evento x", descricao:"Evento de SQL Server" ,data:"10/11/2023"},
+      {id: 2, title: "Evento y", descricao:"Bora codar JS" ,data:"11/11/2023"}
+    ]);
+
     return (
       <MainContent>
         <Banner/>
@@ -14,27 +24,20 @@ const HomePage = () => {
         <Container>
 
       <div className='events-box'>
-      <NextEvent
-    title={"Happy hour Event"}
-    description={"Evento Legal :)"}
-    eventDate={"14/11/23"}
-    idEvento={"eufrgref3rh733"}
-
-      />
-      <NextEvent
-    title={"Happy hour Event"}
-    description={"Evento Legal :)"}
-    eventDate={"14/11/23"}
-    idEvento={"eufrgref3rh733"}
-
-      />
-      <NextEvent
-    title={"Happy hour Event"}
-    description={"Evento Legal :)"}
-    eventDate={"14/11/23"}
-    idEvento={"eufrgref3rh733"}
-
-      />
+        {
+          nextEvents.map((e)=>
+          <NextEvent
+          title={e.title}
+          description={e.descricao}
+          eventDate={e.data}
+          idEvento={e.id}
+      
+            />
+          )
+        }
+   
+    
+    
       </div>
     
 </Container>

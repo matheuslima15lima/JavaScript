29fd94f3e5040912aadcb5f2610c529ce424b02e
Matheus <matheus.cavalcanti5@aluno.senai.br>
2023-11-16 17:21:10 -0300

    feito map na table

--- React/eventplusbase/src/Pages/TipoEventos/TipoEventoPage.jsx
+++ React/eventplusbase/src/Pages/TipoEventos/TipoEventoPage.jsx
@@ -8,10 +8,17 @@ import eventTypeImage from "../../Assets/images/images/tipo-evento.svg";
 import Container from "../../components/Container/Container";
 import { Input, Button } from "../../components/FormComponents/FormComponents";
 import api from "../../Services/Services"
+import TableTp from "./TableTp/TableTp";
 const TipoEventos = () => {
   const [frmEdit, setFrmEdit] = useState(false);
 
   const [titulo, setTitulo] = useState("");
+
+  const [tipoEventos, setTipoEventos] = useState([
+    {idTipoEvento: "123", titulo:"Evento ABC"},
+    {idTipoEvento: "555", titulo:"Evento xpto"},
+    {idTipoEvento: "444", titulo:"Evento de JavaScript"}
+  ]);//array mocado
 
   async function handleSubmit(e) {
     
@@ -28,6 +35,7 @@ const TipoEventos = () => {
       const retorno = await api.post("/TiposEvento", {titulo: titulo})
       console.log("CADASTRADO COM SUCESSO");
       console.log(retorno.data);
+      setTitulo("") //limpa a variavel
     } catch (error) {
       console.log("DEU RUIM NA API");
       console.log(error);
@@ -39,9 +47,21 @@ const TipoEventos = () => {
   function handleUpdate() {
     alert("Bora atualizar");
   }
+  function editActionAbort() {
+    alert("Cancelar a tela de edição de dados");
+  }
+
+  function showUpdateForm(){
+    alert('Mostrando tela de update')
+  }
+  
+  function handleDelete (){
+    alert('Bora lá apagar na api')
+  }
 
   return (
     <MainContent>
+    {/* Cadastro de tipo de eventos */}
       <section className="cadastro-evento-section">
         <Container>
           <div className="cadastro-evento__box">
@@ -92,6 +112,18 @@ const TipoEventos = () => {
               {/*  */}
             </form>
           </div>
+        </Container>
+      </section>
+      {/* LISTAGEM DE TIPO DE EVENTOS */}
+      <section className="lista-eventos-section">
+        <Container>
+          <Title titleText={"Lista Tipo de Eventos"} color="white"/>
+
+          <TableTp
+            dados={tipoEventos}
+            fnUpdate={showUpdateForm}
+            fnDelete={handleDelete}
+          />
         </Container>
       </section>
     </MainContent>
